3f61c71e3b9c9384eb840f77057488ffc5f59017
SuperMoooo <andremontoito@gmail.com>
2024-11-30 18:53:53 +0000

    fix some UI

--- app/components/SkillCard.tsx
+++ app/components/SkillCard.tsx
@@ -9,7 +9,7 @@ interface SkillCardProps {
 
 export default function SkillCard({ name, icon, experience }: SkillCardProps) {
     return (
-        <div className="px-4 py-2 flex items-center  gap-4 rounded-default bg-[#191919] ">
+        <div className="px-4 py-2 flex items-center  gap-4 rounded-default bg-[#191919] w-5/6">
             <picture>
                 <Image
                     src={icon}
@@ -19,9 +19,9 @@ export default function SkillCard({ name, icon, experience }: SkillCardProps) {
                     className="object-cover "
                 />
             </picture>
-            <div className="flex flex-col text-left  justify-center gap-1 w-full">
+            <div className="flex flex-col text-left  justify-center gap-2 w-full">
                 <h1 className="text-2xl font-thin text-accent">{name}</h1>
-                <div className="bg-accent w-full h-[0.8px]"></div>
+                <div className="bg-nonPrimaryText w-full h-[0.8px]"></div>
                 <p className="text-lg text-nonPrimaryText">
                     {parseInt(experience) === 0 ? '> 1 year' : experience}
                 </p>
--- app/components/SkillsSection.tsx
+++ app/components/SkillsSection.tsx
@@ -9,7 +9,7 @@ export default function SkillsSection() {
                 <h1 className="text-4xl">Skills</h1>
                 <div className="bg-accent  md:w-2/5 3/5 h-[0.7px]"></div>
             </div>
-            <section className="grid 2xl:grid-cols-4 xl:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6">
+            <section className="grid 2xl:grid-cols-4 xl:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6 place-items-center">
                 {SKILLS.map((skill) => {
                     return (
                         <SkillCard
